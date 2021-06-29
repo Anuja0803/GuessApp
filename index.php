c38c5c9d1dd2e8b4147b8409a5dc3cc7fb11c2ea
Anuja0803 <73271411+Anuja0803@users.noreply.github.com>
2021-06-29 19:35:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,17 @@
 
 </head>
 <body>
-  <div class="card-panel teal lighten-2"><h4>Welcome to the Anuja's Guessing Game</h4></div>
+  <div class="container">
+    <div class="card-panel teal lighten-2"><h4>Welcome to the Anuja's Guessing Game</h4></div>
       <p>
         <form method="post" class="form-horizontal">
-        <div class="form-group">
-        <div class="col-sm-0 col-sm-offset-2"><h5>Please</h5>
-        <a class="btn btn-default, waves-effect waves-light btn" href="login.php">Login</i></a>
-      </p><br><br></div>
+          <div class="form-group">
+              <div class="col-sm-0 col-sm-offset-2"><h5>Please</h5>
+                <a class="btn btn-default, waves-effect waves-light btn" href="login.php">Login</i></a>
+              </div>
+          </div>
+        </form>
+      </p><br><br>
+    </div>
+  </div>
 </body>
