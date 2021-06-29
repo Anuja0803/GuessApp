42ca2fdef61526e7ba898208b5e7fca045551586
Anuja0803 <73271411+Anuja0803@users.noreply.github.com>
2021-06-29 18:59:34 +0530

    initial commit

---
+++ guess.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>Guessing Game for Anuja Nikam</title>
+<?php require_once "bootstrap.php"; ?>
+<style>
+  label{
+    font-size: 20px;
+    font-weight: bold;
+    padding-top: 30px;
+    color: purple;
+  }
+  input{
+    padding-top: 20px;
+  }
+</style>
+</head>
+<body>
+<h1>Welcome to my guessing game</h1>
+<form method="post" class="form-horizontal">
+  <label>Guess a number between 1 - 100</label>
+  <input type="text" name="guess"/>
+  <input class="btn btn-default, waves-effect waves-light btn" type="submit" name="submit" value="Guess">
+</form>
+<p>
+<?php
+if (isset($_POST['submit'])) {
+  if ( ! isset($_POST['guess']) ) {
+    echo("Missing guess parameter");
+  } else if ( strlen($_POST['guess']) < 1 ) {
+    echo("Your guess is too short");
+  } else if ( ! is_numeric($_POST['guess']) ) {
+    echo("Your guess is not a number");
+  } else if ( $_POST['guess'] < 75 ) {
+    echo("Your guess is too low");
+  } else if ( $_POST['guess'] > 75 ) {
+    echo("Your guess is too high");
+  } else {
+    echo("Congratulations - You are right");
+  }
+}
+
+?>
+</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Anuja Nikam - Guessing Game </title>
+<?php require_once "bootstrap.php"; ?>
+
+</head>
+<body>
+  <div class="card-panel teal lighten-2"><h4>Welcome to the Anuja's Guessing Game</h4></div>
+      <p>
+        <form method="post" class="form-horizontal">
+        <div class="form-group">
+        <div class="col-sm-0 col-sm-offset-2"><h5>Please</h5>
+        <a class="btn btn-default, waves-effect waves-light btn" href="login.php">Login</i></a>
+      </p><br><br></div>
+</body>
